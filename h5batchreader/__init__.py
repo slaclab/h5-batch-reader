948a85ff907cc806f4c7dd88f9033f6f682682b0
David Schneider <davidsch@slac.stanford.edu>
2017-01-26 06:29:44 -0800

    update version string, take out v

--- h5batchreader/__init__.py
+++ h5batchreader/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__='v1.0.0'
+__version__='1.0.0'
 
 from .h5batchreader import H5BatchReader
 from .datasetgroup import DataSetGroup
